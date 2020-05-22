44773d4a50eef969c271060a5473e9262cdab9f0
Aliaksandr Yenza <sashayenza1996@gmail.com>
2020-05-22 14:52:58 -0400

    branch002+

--- src/test/java/TClass1.java
+++ src/test/java/TClass1.java
@@ -1,4 +1,12 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.Thread.sleep;
 
 public class TClass1 {
 
@@ -68,10 +76,65 @@ public class TClass1 {
         }
     }
 
+    @Test
+    public void test008(){
+//        System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");
+//        WebDriver driver = new FirefoxDriver();
+//
+//        String request = "Portnov school";
+//        driver.get("https://www.google.com");
+//
+//        WebElement textInput = driver.findElement(By.cssSelector(".gLFyf"));
+//        textInput.sendKeys(request);
+//        textInput.submit();
+//
+//
+//        try {
+//            sleep(5000);
+//        } catch (InterruptedException e){
+//            e.printStackTrace();
+//        }
+////        WebElement resultValues = driver.findElement(By.id("#result-stats"));
+//
+//        String[] stringArray = driver.findElement(By.id("#result-stats")).getText().split(" ");
+//        int amount  = Integer.parseInt(stringArray[1].replace(",", ""));
+//
+//        Assert.assertTrue(amount > 100);
 
+        System.setProperty("webdriver.gecko.driver", "src/test/resources/geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+
+        String queryString = "Portnov Computer School";
+
+        driver.get("https://www.google.com/");
+
+        WebElement textInput = driver.findElement(By.cssSelector(".gLFyf"));
+
+        textInput.sendKeys(queryString);
+        textInput.submit();
+
+        //TODO: read about try and catch block
+        try {
+            sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement resultsStatsElement = driver.findElement(By.id("result-stats"));
+
+        String resultsStatsText = resultsStatsElement.getText();
+
+        String[] stringArray = resultsStatsText.split(" ");
+
+        String amountOfResults = stringArray[2];
+
+        String amountOfResultsFixed = amountOfResults.replace(",", "");
+
+        int amountOfResultsParsed = parseInt(amountOfResultsFixed);
+
+        Assert.assertTrue(amountOfResultsParsed > 1);
+    }
 
 
 
 }
-
-//1
